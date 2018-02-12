aec4d22212010b4e6d04a2b33bf58af6fe9d5c8b
zhengbei <zhengbei@mowits.com>
2018-02-12 09:58:07 +0800

    测试提交

---
+++ 04_Source/01_Working/web-parent/web-api/src/main/java/com/babynorth/web/entity/test.java
@@ -0,0 +1,8 @@
+package com.babynorth.web.entity;
+
+/**
+ * @author zhengbei
+ * @date 2018/2/12.
+ */
+public class test {
+}
---
+++ 04_Source/01_Working/web-parent/web-api/src/main/java/com/babynorth/web/service/test.java
@@ -0,0 +1,8 @@
+package com.babynorth.web.service;
+
+/**
+ * @author zhengbei
+ * @date 2018/2/12.
+ */
+public class test {
+}
